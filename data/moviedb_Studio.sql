95bec3633f3961b210a7f070d579844259798b10
Florian Reiter <flow.reiter@gmail.com>
2016-11-01 10:18:25 +0100

    Data Added, gitignore updated

---
+++ data/moviedb_Studio.sql
@@ -0,0 +1,7 @@
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('CA', 'Universal', 91608);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('CA', '20th Century Fox', 90064);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('NY', 'RKO Pictures', 10020);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('CA', 'Paramount Picture', null);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('CA', 'Warner Bros.', null);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('BH', 'Metro-Goldwyn-Mayer', null);
+INSERT INTO moviedb.Studio (COUNTRYCODE, NAME, POSTCODE) VALUES ('CC', 'Columbia Pictures', null);
